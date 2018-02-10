ed5522ca332d14d2156862109cc41443949ffc89
Benjamin Grange <benjamin.grange@epitech.eu>
2018-02-10 23:55:37 +0100

    Added better help message.

--- src/args.rs
+++ src/args.rs
@@ -24,9 +24,8 @@ impl Args {
         opts.optflag("h", "help", "print this help menu");
         opts.optflag("v", "version", "print koak's version");
         opts.optflag("l", "lexer", "stops after lexing, dumping the generated tokens");
-        opts.optflag("O", "optimization", "enables various optimizations");
-
         opts.optflag("p", "parser", "stops after parsing, dumping the generated AST");
+        opts.optflag("O", "optimization", "enables various optimizations");
 
         let matches = match opts.parse(&args[1..]) {
             Ok(m) => m,
@@ -38,7 +37,8 @@ impl Args {
 
         // Quick help & version handlers
         if matches.opt_present("h") {
-            println!("{} [FLAGS] [FILE...]", prog_name);
+            let f = format!("{} [options] [file]", prog_name);
+            println!("{}", opts.usage(&f));
             process::exit(0);
         }
         else if matches.opt_present("v") {
